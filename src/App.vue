fec2f5d5cd43cc808f835f7ac8a0fb6535da0a08
Caspar Danielsson <caspar.danielsson@iths.se>
2025-02-20 14:01:19 +0100

    iterate all decks from decks.json

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
 import { RouterView } from "vue-router";
 import FlashCard from "./components/FlashCard.vue";
-import CardView from "./components/CardView.vue";
 
 </script>
 
 <template>
-  <h1>hej</h1>
-  <FlashCard />
   <RouterView />
 </template>
 
--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -1,22 +1,24 @@
-<script>
-  export default {
-    created() {
-      fetch("https://avancera.app/cities/")
-        .then((response) => response.json())
-        .then((cities) => {
-          this.cities = cities;
-        });
-    },
-    data() {
-      return {
-        cities: null
-      };
-    }
-  };
+<script setup>
+import { ref, onMounted } from "vue";
+
+import Swiper from "swiper";
+import "swiper/css";
+
+async function getDecks() {
+  const response = await fetch("/decks.json");
+  let data = await response.json();
+  decks.value = data
+}
+
+const decks = ref([]);
+getDecks()
+
+console.log(decks.value);
 </script>
 
 <template>
+  <h1>hej från flashcard</h1>
   <ol>
-    <li v-for="city in cities">{{ city.name }}</li>
+    <li v-for="deck in decks">{{ deck.deckName }}</li>
   </ol>
 </template>
