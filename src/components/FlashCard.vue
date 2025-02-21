edb45180a7da620e992ad89c10d1aec236b2710d
Caspar Danielsson <caspar.danielsson@iths.se>
2025-02-21 11:28:40 +0100

    on click event reveal answer

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { useRoute } from "vue-router"
-import { ref, watchEffect, onMounted} from "vue";
-import { onBeforeRouteUpdate } from "vue-router"
+import { useRoute } from "vue-router";
+import { ref, watchEffect, onMounted } from "vue";
+import { onBeforeRouteUpdate } from "vue-router";
 
-const route = useRoute()
+const route = useRoute();
 const decks = ref([]);
-const currentDeck = ref([])
-const currentCard= ref([])
-let deckId = null
-let cardNr = null
+const currentDeck = ref([]);
+const currentCard = ref([]);
+const hideAnswer = ref(true);
+let deckId = null;
+let cardNr = null;
 
 /** Fetch decks fron public/decks.json */
 async function getDecks() {
@@ -17,58 +18,59 @@ async function getDecks() {
   decks.value = data;
 }
 
+/** */
+function revealAnswer(){
+  hideAnswer.value = false
+  console.log("reveal")
+}
+
 // Update deckId and cardId when page refreshes
 onMounted(async () => {
-  await getDecks()
+  await getDecks();
 
   watchEffect(
     () => [route.params.deckId, route.params.cardNr],
-    currentDeck.value = decks.value[route.params.deckId],
-    currentCard.value = currentDeck.value.cards[route.params.cardNr],
+    (currentDeck.value = decks.value[route.params.deckId]),
+    (currentCard.value = currentDeck.value.cards[route.params.cardNr]),
     console.log(currentDeck.value),
     console.log(currentCard.value)
-  )
-})
+  );
+});
 
 // Update deckId and cardId when url changes
 onBeforeRouteUpdate(async (to, from) => {
-  deckId = to.params.deckId
-  cardNr = to.params.cardNr
-  currentDeck.value = decks.value[deckId]
-  currentCard.value = currentDeck.value.cards[cardNr]
-})
+  deckId = to.params.deckId;
+  cardNr = to.params.cardNr;
+  currentDeck.value = decks.value[deckId];
+  currentCard.value = currentDeck.value.cards[cardNr];
+});
+
 </script>
 
 <template>
-  <div id="center">
-
+  <div id="center" @click="revealAnswer">
     <!-- Get card from url parameters -->
-    <h1>{{currentDeck.title}}</h1>
-    <div class="card" id="front">
-      fråga: {{currentCard.question}}
+    <h1>{{ currentDeck.title }}</h1>
+    <div v-if="hideAnswer" class="card" id="front">fråga: {{ currentCard.question }}</div>
+    <div v-else class="card" id="back">svar: {{ currentCard.answer }}</div>
   </div>
-  <div class="card" id="back">
-    svar: {{currentCard.answer}}
-  </div>
-</div>
 </template>
 
 <style scoped>
-  #center {
-    width: 100%;
-    border: solid red;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .card {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: clamp(9em, 95%, 33em);
-    height: min(31em, 143vw);
-    background-color: #f9ffef;
-    box-shadow: 0 0 0 0 #000;
-    border-radius: 10px;
-  }
+#center {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: clamp(9em, 95%, 33em);
+  height: min(31em, 143vw);
+  background-color: #f9ffef;
+  box-shadow: 0 0 0 0 #000;
+  border-radius: 10px;
+}
 </style>
