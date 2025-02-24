5409d9e2de3894b9ad4b74c0034c5bfaa13973ce
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-02-24 12:50:49 +0100

    add green/red button

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,7 +1,29 @@
 <script setup>
-  import FlashCard from '../components/FlashCard.vue';
+import FlashCard from "../components/FlashCard.vue";
+import FlashcardButton from "../components/FlashcardButton.vue";
 </script>
 
 <template>
   <FlashCard />
+  <div class="center">
+    <div class="buttons">
+      <FlashcardButton color="var(--success)" text="Rätt" />
+      <FlashcardButton color="var(--danger)" text="Fel" />
+    </div>
+  </div>
 </template>
+
+<style>
+.center {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.buttons {
+  display: flex;
+  justify-content: space-between;
+  width: clamp(9em, 95%, 33em);
+  margin-top: 20px;
+}
+</style>
