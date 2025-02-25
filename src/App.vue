650cbbc29eb89f1f640e6dd31453d757bf018801
erikssonelin <elin.frida.elisabet.eriksson@gmail.com>
2025-02-25 06:57:49 +0100

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Navbar from "./components/Navbar.vue";
 import { RouterView } from "vue-router";
 import FlashCard from "./components/FlashCard.vue";
 
@@ -6,26 +7,19 @@ import CardCollection from "./components/CardCollection.vue";
 import CardView from "./views/CardView.vue";
 
 import { useFlashcard } from "./stores/flashcards";
-const flashcards = useFlashcard()
+const flashcards = useFlashcard();
 
 // Inits all decks
 // Skapa kortlekar => [] dummy
-flashcards.decks = flashcards.dummyDeck()
+flashcards.decks = flashcards.dummyDeck();
 
-console.log(flashcards.decks)
-
-
-
-
-
+console.log(flashcards.decks);
 </script>
 
 <template>
+  <Navbar />
   <RouterView />
   <!-- <CardCollection />> -->
 </template>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
