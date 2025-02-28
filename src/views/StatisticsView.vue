4918d2599c681f72fec569c091f3993ef2037f3a
Caspar Danielsson <caspar.danielsson@iths.se>
2025-02-28 14:36:50 +0100

    add StatisticView

---
+++ src/views/StatisticsView.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { useFlashcard } from "../stores/flashcards";
+  import { ref } from "vue";
+  const flashcard = useFlashcard();
+  const stats = ref("in stats");
+</script>
+
+<template>
+  <main>
+    <h1>{{ stats }}</h1>
+  </main>
+</template>
+
+<style scoped></style>
