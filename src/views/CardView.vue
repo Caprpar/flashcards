cfb158b5191d78121fd58014dacff57d515fe9e5
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-02-28 09:26:58 +0100

    cleanup

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -137,29 +137,4 @@
     border: none;
     padding: 8px;
   }
-  <style scoped > main {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
-  }
-
-  .card-view-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 90vh;
-    padding-top: 2em;
-    width: clamp(9em, 95%, 43em);
-  }
-  .buttons-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-top: 20px;
-    gap: 10px; /* Adds space between buttons */
-    width: 100%; /* Ensures it fills the container */
-    max-width: 700px; /* Adjust this as needed */
-  }
 </style>
