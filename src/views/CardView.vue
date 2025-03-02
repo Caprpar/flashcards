73221ecb4b6a16819ea0a09a305793da48487565
Caspar Danielsson <caspar.danielsson@iths.se>
2025-03-02 21:38:31 +0100

    Remove old arrows from previous merge conflict

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -52,6 +52,33 @@
 </template>
 
 <style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    flex-direction: column;
+  }
+
+  .card-view-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+    padding-top: 2em;
+    width: clamp(9em, 95%, 43em);
+  }
+  .buttons-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 20px;
+    gap: 10px; /* Adds space between buttons */
+    width: 100%; /* Ensures it fills the container */
+    max-width: 700px; /* Adjust this as needed */
+  }
+
   #answer-indicator {
     margin-top: 1em;
     justify-content: space-around;
