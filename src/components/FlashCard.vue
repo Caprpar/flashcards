7e4d4c6e3b69ebf87648ad7c4da27214143e46e8
Ingemar Berghult <ingemar.kjellman@gmail.com>
2025-03-06 14:06:08 +0100

    arrow skip card bug fix

--- src/components/FlashCard.vue
+++ src/components/FlashCard.vue
@@ -24,7 +24,7 @@
   const route = useRoute();
   const currentDeck = ref([]);
   const currentCard = ref([]);
-  const cardNr = ref(route.params.cardNr - 1);
+  const cardNr = ref(route.params.cardNr);
   let deckId = route.params.deckId;
 
   let show = ref(true);
